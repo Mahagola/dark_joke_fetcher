01184724df4ca730543c1318fdadca26397466d5
Suryansh <104053578+Mahagola@users.noreply.github.com>
2023-06-16 20:49:39 +0530

    added files

---
+++ script.js
@@ -0,0 +1,19 @@
+// Fetch jokes from the API
+function get_jokes(){
+    fetch('https://v2.jokeapi.dev/joke/Dark?type=twopart&amount=10')
+        .then(response => response.json())
+        .then(data => {
+            let jokes = data.jokes;
+            let temp = document.querySelector('.setup');
+            let index = Math.floor(Math.random() * jokes.length);
+            let joke = jokes[index];
+            temp.textContent = joke.setup;
+            temp = document.querySelector('.delivery');
+            temp.textContent = joke.delivery;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+get_jokes()
+
